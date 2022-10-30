7540477dcb7cc059489479e3fad770de215524fc
Leochen2126 <leochen2126@gmail.com>
2022-10-29 21:34:21 -0400

    Update riot.service.js

--- BackEnd/server/services/riot.service.js
+++ BackEnd/server/services/riot.service.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import apiKey from
 
 //example url
 const API_URL = "https://americas.api.riotgames.com/lol/match/v5/matches/NA1_4442533965?api_key=RGAPI-b73e0c69-d38b-4436-b01c-94d8c6c705f9";
